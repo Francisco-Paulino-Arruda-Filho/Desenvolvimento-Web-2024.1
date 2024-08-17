9eefbc30b33a2f2561198cca19b2c7b3a5622c61
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-08-17 13:50:38 -0300

    Revertendo mudanças

--- Desenvolvimento-Web-2024.1/src/App.tsx
+++ Desenvolvimento-Web-2024.1/src/App.tsx
@@ -1,18 +1,8 @@
 import './App.css'
-import Questao01A from './components/AP1/Questao1'
-import Questao02 from './components/AP1/Questao2'
-import Questao03 from './components/AP1/Questao03.tsx'
-import Questao04 from './components/AP1/Questao04.tsx'
-import Questao04B from './components/AP1/Questao4B.tsx'
 
 function App() {
   return (
     <>
-      <Questao01A/>
-      <Questao02/>
-      <Questao03/>
-      <Questao04/>  
-      <Questao04B/>
     </>
   )
 }
